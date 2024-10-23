76551a930ff91ed92a977ea69f25ffd0c347f0de
Fabriluc <fabriluc03@gmail.com>
2024-10-23 00:39:33 -0300

    Added Models for E-commerce

---
+++ src/main/java/com/tequila/ecommerce/vinoteca/models/Cart.java
@@ -0,0 +1,49 @@
+package com.tequila.ecommerce.vinoteca.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToMany(mappedBy = "cart")
+    private List<Product> products = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+}
---
+++ src/main/java/com/tequila/ecommerce/vinoteca/models/Favorites.java
@@ -0,0 +1,56 @@
+package com.tequila.ecommerce.vinoteca.models;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "favorites")
+public class Favorites {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "favorites_products",
+            joinColumns = @JoinColumn(name = "favorites_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+}
