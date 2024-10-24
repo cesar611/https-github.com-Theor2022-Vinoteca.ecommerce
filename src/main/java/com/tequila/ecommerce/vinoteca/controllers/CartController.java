2f10b03258d458ef5c8e70a1dd0f0cd95c07b3eb
Agustin <pradoagustin505@gmail.com>
2024-10-24 00:42:00 -0300

    controlado carrito

--- src/main/java/com/tequila/ecommerce/vinoteca/controllers/CartController.java
+++ src/main/java/com/tequila/ecommerce/vinoteca/controllers/CartController.java
@@ -1,4 +1,33 @@
 package com.tequila.ecommerce.vinoteca.controllers;
 
+import com.tequila.ecommerce.vinoteca.models.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+    @RestController
+    @RequestMapping("/api/carrito")
 public class CartController {
+
+  @Autowired
+  private CartService CartService;
+
+  @PostMapping
+  public ResponseEntity<Cart> crearCarrito(@RequestBody Cart carrito) {
+    Cart nuevoCarrito = CartService.CrearCart(carrito);
+    return ResponseEntity.ok(nuevoCarrito);
+  }
+
+  @PutMapping
+  public ResponseEntity<Object> modificarCarrito(@RequestBody Cart carrito) {
+    Cart carritoModificado = CartService.modificarCart(carrito);
+    return ResponseEntity.ok(carritoModificado);
+  }
+  @PutMapping
+      public ResponseEntity<Object> agregarProducto(@RequestBody Cart carrito) {
+    Cart carritoModificado = CartService.agregarProducto(carrito);
+    return ResponseEntity.ok(carritoModificado);
+  }
 }
+
---
+++ src/main/java/com/tequila/ecommerce/vinoteca/controllers/CartService.java
@@ -0,0 +1,18 @@
+package com.tequila.ecommerce.vinoteca.controllers;
+
+import com.tequila.ecommerce.vinoteca.models.Cart;
+
+public class CartService {
+    public Cart CrearCart(Cart carrito) {
+        return carrito;
+    }
+
+    public Cart modificarCart(Cart carrito) {
+        return carrito;
+    }
+
+
+    public Cart agregarProducto(Cart carrito) {
+        return null;
+    }
+}
